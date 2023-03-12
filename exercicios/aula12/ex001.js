1c302a28d857ad8c28ed9f8554efa5590a1c23d5
FelipeBaccho <119887404+FelipeBaccho@users.noreply.github.com>
2023-03-12 18:30:52 -0300

    Correcoes aulas 10 e 11

--- exercicios/aula12/ex001.js
+++ exercicios/aula12/ex001.js
@@ -17,6 +17,6 @@ if (idade < 16) {
     console.log('Não vota.')
 } else if (idade < 18 || idade >= 65) {
     console.log('Voto opcional.')
-} else {
+} else {3
     console.log('Voto obrigatório.')
 }
--- exercicios/aula12/ex002.js
+++ exercicios/aula12/ex002.js
@@ -7,6 +7,6 @@ if (hora < 6) {
     console.log('Bom dia!')
 } else if (hora <= 18) {
     console.log('Boa tarde!')
-} else {
+} else { 
     console.log('Boa noite!')
 }
